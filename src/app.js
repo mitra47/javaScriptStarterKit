79abe70d701cf186317f183a4727847135a0ebcc
mitra47 <73024958+mitra47@users.noreply.github.com>
2021-10-19 00:01:56 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log ("Merhaba Kodlama.io")
+//JS type sate degildir
+
+let dolarDun =9.20
+
+let dolarBugun =9.30
+
+
+
+{
+    let dolarDun = 9.10
+}
+
+
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun = 11 bu yapılmaz 
+
+
+//array [] kullanılır
+//camelCasing (degişken Yapısı ilk harf küçük)
+//PascalCasing (değişken yapısı ilk harf Büyük yazılır)
+let konutKredileri = ["Konut Kredisi", " Konut Kredesi" , "Araç Kredisi"]
+console.log("<ul>")
+for (let index = 0;i<konutKredileri.length;i++) {
+    console.log("<li>"+ konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+console.log(konutKredileri)
+
+
+
